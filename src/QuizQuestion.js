9aadaf1a702ca76af434fb715cfdb14b9b7a604c
SebastianC <ciobanu.sebastian96@yahoo.com>
2019-05-21 17:03:59 +0300

    Added answer check & error.

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,12 +2,28 @@ import React, {Component} from 'react';
 import QuizQuestionButton from "./QuizQuestionButton";
 
 class QuizQuestion extends Component {
+    constructor(props) {
+        super(props);
+        this.state= {incorrectAnswer: false}
+    }
+
     handleClick(buttonText) {
-        (buttonText === this.props.quiz_question.answer) ? this.props.showNextQuestionHandler() : ''
+        if(buttonText === this.props.quiz_question.answer) {
+            this.props.showNextQuestionHandler();
+            this.setState(_ => {
+                return {incorrectAnswer: false}
+            })
+        } else {
+            this.setState(_ => {
+                return {incorrectAnswer: true}
+            })
+        }
+
     }
     render() {
         return (
             <main>
+                {this.state.incorrectAnswer ? <p className="error">Sorry, that's not right</p> : null}
                 <section>
                     <p>{this.props.quiz_question.instruction_text}</p>
                 </section>
