4a6da978c61093f5a304ecf8c0fba3ca53c5a582
SebastianC <ciobanu.sebastian96@yahoo.com>
2019-05-21 16:49:00 +0300

    Added click handlers.

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,6 +2,9 @@ import React, {Component} from 'react';
 import QuizQuestionButton from "./QuizQuestionButton";
 
 class QuizQuestion extends Component {
+    handleClick(buttonText) {
+        (buttonText === this.props.quiz_question.answer) ? this.props.showNextQuestionHandler() : ''
+    }
     render() {
         return (
             <main>
@@ -10,7 +13,11 @@ class QuizQuestion extends Component {
                 </section>
                 <section className="buttons">
                     <ul>
-                        <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]}/>
+                        {
+                            this.props.quiz_question.answer_options.map((answer_option, index) =>
+                                <QuizQuestionButton button_text={answer_option} key={index} clickHandler={this.handleClick.bind(this)}/>
+                            )
+                        }
                     </ul>
                 </section>
             </main>
